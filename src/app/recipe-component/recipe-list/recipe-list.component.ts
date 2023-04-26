25f02f0145dbf1aa4d5346a74df9cd4ba0882bd6
Vedonic-BND <vdn3903@students.uc-bcf.edu.ph>
2023-04-26 09:02:51 +0800

    add recipe list items

--- src/app/recipe-component/recipe-list/recipe-list.component.ts
+++ src/app/recipe-component/recipe-list/recipe-list.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html'
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Name', 'Test Desc', 'https://picsum.photos/1920/1080/?random'),
+    new Recipe('Test Name', 'Test Desc', 'https://picsum.photos/1920/1080/?random'),
 
+  ];
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
 };

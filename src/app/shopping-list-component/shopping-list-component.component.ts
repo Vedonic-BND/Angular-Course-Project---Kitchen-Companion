f889c16e48f5d7337be7af5691e779e82c16dae7
Vedonic-BND <vdn3903@students.uc-bcf.edu.ph>
2023-04-26 20:20:58 +0800

    fix shopping list components

---
+++ src/app/shopping-list-component/shopping-list-component.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-list-component',
+  templateUrl: './shopping-list-component.component.html'
+})
+export class ShoppingListComponentComponent implements OnInit{
+  ingredients = [];
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+}

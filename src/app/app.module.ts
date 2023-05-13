d1d0cac762ed74ec873e8a7737c009aa69b41b6f
Vedonic-BND <vdn3903@students.uc-bcf.edu.ph>
2023-05-13 13:01:03 +0800

    not so sure fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FormsModule } from '@angular/forms';
-import { RecipeComponentComponent } from './recipe-component/recipe-component.component';
-import { RecipeDetailComponent } from './recipe-component/recipe-detail/recipe-detail.component';
-import { RecipeListComponent } from './recipe-component/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipe-component/recipe-list/recipe-item/recipe-item.component';
-import { ShoppingListEditComponent } from './shopping-list-component/shopping-list-edit/shopping-list-edit.component';
-import { ShoppingListComponentComponent } from './shopping-list-component/shopping-list-component.component';
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { RecipeComponentComponent } from "./recipe-component/recipe-component.component";
+import { RecipeDetailComponent } from "./recipe-component/recipe-detail/recipe-detail.component";
+import { RecipeItemComponent } from "./recipe-component/recipe-list/recipe-item/recipe-item.component";
+import { RecipeListComponent } from "./recipe-component/recipe-list/recipe-list.component";
+import { DropdownDirective } from "./shared/dropdown.directive";
+import { ShoppingListComponentComponent } from "./shopping-list-component/shopping-list-component.component";
+import { ShoppingListEditComponent } from "./shopping-list-component/shopping-list-edit/shopping-list-edit.component";
 
 @NgModule({
   declarations: [
@@ -20,7 +20,8 @@ import { ShoppingListComponentComponent } from './shopping-list-component/shoppi
     RecipeListComponent,
     RecipeItemComponent,
     ShoppingListEditComponent,
-    ShoppingListComponentComponent
+    ShoppingListComponentComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
